b0d6b460972ce9d80769e5dedb916f42d33122d1
princesingh8178 <161611206+princesingh8178@users.noreply.github.com>
2025-02-22 16:46:26 +0530

    commit the footer

---
+++ src/components/Footor.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer className="bg-gray-900 text-white text-center p-4 mt-10">
+        <p>© 2025 Body Maker. All Rights Reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
